dc2ad304763a78ded6bd29e4b5a42eb2b74c5ead
salihcelebi96 <celebisalih277@gmail.com>
2023-12-20 01:23:30 +0300

    10.commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteStyleImport from 'vite-plugin-style-import';
 
-
-
-export default {
+export default defineConfig({
+  plugins: [
+    react(),
+    ViteStyleImport({
+      libs: [
+        {
+          libraryName: 'slick-carousel',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `slick-carousel/slick/${name}.css`;
+          },
+        },
+      ],
+    }),
+  ],
   server: {
     port: 3000,
-    
   },
- 
-
-};
+});
