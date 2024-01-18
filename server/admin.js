b2acc784d9b5afd6a05bfb3ee5245fb06c63a3ca
salihcelebi96 <celebisalih277@gmail.com>
2024-01-18 18:24:27 +0300

    38.commit

--- server/admin.js
+++ server/admin.js
@@ -5,53 +5,36 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
-
-
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
-
+app.use(cors()); // Sadece bu satır yeterli
 
 mongoose.connect('mongodb+srv://celebisalih277:salih266@cluster0.4wktsa2.mongodb.net/PizzaHut', {
   tls: true,
   tlsAllowInvalidCertificates: true
-}
-
-);
+});
 
 const adminSchema = new mongoose.Schema({
-    email: {
-      type: String, 
-      required: true,
-    },
-    password: {
-      type: String, 
-      required: true,
-    },
-  });
-  
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-
-const admins = mongoose.model('admin',adminSchema); 
-
-// Express ortamı
+const admins = mongoose.model('admin', adminSchema);
 
 app.use(express.json());
 
-// API endpoint'i
-app.get('/admin', async (req, res) => { 
+app.get('/admin', async (req, res) => {
   try {
-    const data = await admins.find(); 
+    const data = await admins.find();
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
