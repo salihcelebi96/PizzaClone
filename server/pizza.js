38057be29dc172c8c0eb7555ab061caa1c4dffba
salihcelebi96 <celebisalih277@gmail.com>
2024-01-18 18:54:26 +0300

    38.commit

--- server/pizza.js
+++ server/pizza.js
@@ -10,12 +10,7 @@ app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
+
 
 
 
