735a048a2be792b30d87854f3d1bf31dab9333d6
salihcelebi96 <celebisalih277@gmail.com>
2024-01-21 04:31:30 +0300

    75.commit

--- server/icecekler.js
+++ server/icecekler.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const redis = require('redis');
 const dotenv = require('dotenv');
 
 const app = express();
@@ -37,33 +36,10 @@ const Icecekler = mongoose.model('icecek', icecekSchema);
 
 app.use(express.json());
 
-// Redis konfigürasyonu
-const redisClient = redis.createClient({
-  host: 'localhost', // Redis sunucu adresi
-  port: 6379,         // Redis sunucu portu
-  // password: 'your_password', // Eğer Redis sunucu yetkilendirme gerektiriyorsa, password alanını ekleyin
-});
-
 app.get('/icecekler', async (req, res) => {
   try {
-    // Önce Redis'te veriyi ara
-    redisClient.get('icecekler', async (err, cachedData) => {
-      if (err) throw err;
-
-      if (cachedData) {
-        // Eğer Redis'te varsa, Redis'ten veriyi çek
-        const parsedData = JSON.parse(cachedData);
-        res.json(parsedData);
-      } else {
-        // Eğer Redis'te yoksa, MongoDB'den veriyi çek
-        const data = await Icecekler.find();
-        
-        // Veriyi Redis'e ekle
-        redisClient.setex('icecekler', 3600, JSON.stringify(data));
-
-        res.json(data);
-      }
-    });
+    const data = await Icecekler.find();
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -78,9 +54,6 @@ app.post('/icecekler', async (req, res) => {
       fiyat,
       url,
     });
-
-    // Eğer yeni bir içecek eklenirse, Redis önbelleğini geçersiz kıl
-    redisClient.del('icecekler');
 
     res.status(201).json(savedIcecek);
   } catch (error) {
