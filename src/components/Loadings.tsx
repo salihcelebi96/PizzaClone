db93e5b67f4105a3dd42485ff31317cf51f54c8d
salihcelebi96 <celebisalih277@gmail.com>
2024-02-20 11:45:34 +0300

    104.commit

--- src/components/Loadings.tsx
+++ src/components/Loadings.tsx
@@ -3,7 +3,7 @@ import { BounceLoader } from 'react-spinners';
 
 const Loadings: React.FC = () => {
   return (
-    <div className='h-screen flex justify-center items-center'>
+    <div className='h-screen w-screen flex justify-center items-center'>
       <BounceLoader color="#36d7b7" size={30} />
     </div>
   );
