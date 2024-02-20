6fdcaa357006d59772e8799e30cb6c2c51d63776
salihcelebi96 <celebisalih277@gmail.com>
2024-02-20 13:35:11 +0300

    105.commit

--- src/components/SlideSmall.tsx
+++ src/components/SlideSmall.tsx
@@ -6,6 +6,7 @@ import foto1 from "../assets/slide/foto1.jpg";
 import foto2 from "../assets/slide/foto2.jpg";
 import foto3 from "../assets/slide/foto3.jpg";
 import foto4 from "../assets/slide/foto4.jpg";
+import { Link } from 'react-router-dom';
 
 const Slide = () => {
   const settings = {
@@ -27,12 +28,15 @@ const Slide = () => {
         <Slider {...settings}>
           {slideImages.map((slideImage, index) => (
             <div className='h-full ' key={index}>
+              <Link to="/kampanyalar">
               <img
               className='h-[300px]  '
                 src={slideImage}
                 alt={`Slide ${index + 1}`}
                 
               />
+              </Link>
+              
             </div>
           ))}
         </Slider>
